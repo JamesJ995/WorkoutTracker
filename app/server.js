425d5987bda22e2ca21bb2fd41b3f5c2ad8649c0
jamesj995 <mrjam7@gmail.com>
2021-10-02 19:49:37 -0500

    fixing path

--- app/server.js
+++ app/server.js
@@ -1,30 +1,28 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const logger = require('morgan')
+const express = require('express');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/workoutDB',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  }
-);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workoutDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require('./routes/api.js'));
 
 app.get('*', function (req, res) {
   const index = path.join(__dirname, 'build', 'index.html');
